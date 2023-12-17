79218710c22be9a9ee20373df2a8c62b3e911187
longpv <longpv@codluck.com>
2023-12-17 12:48:04 +0700

    function template

--- wp-content/themes/wplongpv/inc/template-functions.php
+++ wp-content/themes/wplongpv/inc/template-functions.php
@@ -120,3 +120,95 @@ function custom_img($post_id = null, $img_url = null)
 
     echo $image;
 }
+
+// block info general information
+function block_info($data_block = [])
+{
+    $html = '';
+
+    if ($data_block) {
+        $data = [
+            'title' => $data_block['title'] ?? null,
+            'desc' => $data_block['description'] ?? null,
+            'link' => $data_block['link'] ?? [],
+            'layout' => $data_block['display_type'] ?? 'default',
+        ];
+
+        // render html the section
+        $html = '<div class="secHeading' . (($data['layout'] == 'center') ? ' secHeading--center ' : '') . '">';
+        $html .= $data['layout'] == 'center' ? '<div class="container"><div class="row no-gutters justify-content-center"><div class="col-lg-8">' : '';
+        $html .= $data['title'] ? '<h2 class="secTitle secHeading__title">' . custom_title($data['title']) . '</h2>' : '';
+        $html .= $data['desc'] ? '<p class="secHeading__desc">' . $data['desc'] . '</p>' : '';
+        $html .= custom_btn_link($data['link'], 'secHeading__link', true);
+        $html .= $data['layout'] == 'center' ? '</div></div></div>' : '';
+        $html .= '</div>';
+    }
+
+    return $html;
+}
+// end block info
+
+// validate list data
+function validate_data($data = [], $comparison = 'OR')
+{
+    $validate = '';
+    $comparison = strtoupper($comparison);
+
+    if ($data) {
+        foreach ($data as $field) {
+            $validate .= $field ? '-true' : '-false';
+        }
+    }
+
+    $check = strpos($validate, $comparison === "AND" ? 'false' : 'true');
+
+    return $check ? (($comparison === "AND") ? false : true) : (($comparison === "AND") ? true : false);
+}
+
+// block editor general
+function custom_editor($content = null, $class = null)
+{
+    return $content ? '<div class="editor ' . ($class ?: '') . '">' . $content . '</div>' : '';
+}
+
+// block btn link general
+function custom_btn_link($link = [], $class = null, $block = false)
+{
+    $html = '';
+
+    if ($link) {
+        // validate link
+        $url = ($link && $link['url']) ? $link['url'] : 'javascript:void(0);';
+        $title = ($link && $link['title']) ? $link['title'] : __('See more', 'clvinuni');
+        $target = ($link && $link['target']) ? $link['target'] : '_self';
+        $class_link = !$block ? ($class ? $class : '') : '';
+
+        // renter html
+        $html .= $block ? '<div class="' . $class . '">' : '';
+        $html .= '<a href="' . $url . '" target="' . $target . '" class="btnSeeMore ' . $class_link . '">';
+        $html .= $title;
+        $html .= '</a>';
+        $html .= $block ? '</div>' : '';
+    }
+
+    return $html;
+}
+
+// block image link general
+function custom_img_link($link = [], $image = null, $class = null, $alt = null)
+{
+    $html = '';
+
+    // validate link
+    $url = ($link && $link['url']) ? $link['url'] : 'javascript:void(0);';
+    $title = ($link && $link['title']) ? $link['title'] : __('See more', 'clvinuni');
+    $target = ($link && $link['target']) ? $link['target'] : '_self';
+    $class_block = ($link && $link['url']) ? ($class ? $class : '') : 'cursor-default';
+
+    // renter html
+    $html .= '<a class="imgGroup ' . $class_block . '" href="' . $url . '" target="' . $target . '">';
+    $html .= '<img width="300" height="300" src="' . custom_img(null, $image) . '" alt="' . ($alt ?: $title) . '">';
+    $html .= '</a>';
+
+    return $html;
+}
