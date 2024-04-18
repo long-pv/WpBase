2d75f3e9aeeaf30a2a27965bfe3a24e30cf146eb
longpv <longpv@codluck.com>
2024-04-18 22:36:52 +0700

    thay đổi lại section block

--- wp-content/themes/wplongpv/template-parts/content-flexible.php
+++ wp-content/themes/wplongpv/template-parts/content-flexible.php
@@ -2,15 +2,14 @@
 $flexible_content = get_field('flexible_content') ?? '';
 
 if ($flexible_content):
-	foreach ($flexible_content as $flexible):
+	foreach ($flexible_content as $key => $flexible):
 		// custom class
 		$customClass = 'secSpace ';
 		$sectionId = custom_name_block($flexible['acf_fc_layout']);
 		$customClass .= $sectionId;
-		$customClass .= '  ';
-		$customClass .= $flexible['block_info']['custom_class'] ?? '';
+		$customClass .= !empty($flexible['block_info']['custom_class']) ? ' ' . $flexible['block_info']['custom_class'] : '';
 		?>
-		<section class="<?php echo $customClass; ?>" id="<?php echo $sectionId; ?>">
+		<section class="<?php echo $customClass; ?>" id="<?php echo $sectionId . '_' . $key; ?>">
 			<?php
 			$args['flexible'] = $flexible;
 			get_template_part('template-parts/block/' . $flexible['acf_fc_layout'], '', $args);
