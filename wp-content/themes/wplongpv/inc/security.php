767b54c11327a28f6851559d53a3b4a588cdea78
LongPV <longpv@codluck.com>
2024-05-02 09:07:07 +0700

    update các phần k cần thiết

--- wp-content/themes/wplongpv/inc/security.php
+++ wp-content/themes/wplongpv/inc/security.php
@@ -75,10 +75,7 @@ function disable_comments_and_pings_post_type()
         $cerber_antispam_value = get_option('cerber-antispam') ?? [];
         $cerber_antispam_value['botsreg'] = 1;
         $cerber_antispam_value['botscomm'] = 1;
-        $cerber_antispam_value['botsany'] = 0;
-        $cerber_antispam_value['botssafe'] = 0;
         $cerber_antispam_value['botsipwhite'] = 0;
-        $cerber_antispam_value['botsnoauth'] = 0;
         $cerber_antispam_value['spamcomm'] = 1;
         $cerber_antispam_value['trashafter-enabled'] = 1;
         $cerber_antispam_value['trashafter'] = 3;
@@ -99,19 +96,16 @@ function disable_comments_and_pings_post_type()
         $cerber_hardening_value['norest'] = 1;
         $cerber_hardening_value['restauth'] = 1;
         $cerber_hardening_value['restroles'] = ['administrator'];
-        $cerber_hardening_value['restwhite'] = ['oembed', 'wp-site-health', 'contact-form-7'];
         update_option('cerber-hardening', $cerber_hardening_value);
 
         // cerber-main
         $cerber_main_value = get_option('cerber-main') ?? [];
-        $cerber_main_value['limitwhite'] = 0;
         $cerber_main_value['nologinhint'] = 1;
         $cerber_main_value['nopasshint'] = 1;
         $cerber_main_value['nologinlang'] = 1;
         $cerber_main_value['noredirect'] = 1;
         $cerber_main_value['subnet'] = 1;
         $cerber_main_value['proxy'] = 1;
-        $cerber_main_value['cerber_sw_repo'] = 0;
         $cerber_main_value['citadel_on'] = 1;
         $cerber_main_value['cinotify'] = 1;
         $cerber_main_value['cerberlab'] = 1;
@@ -121,8 +115,6 @@ function disable_comments_and_pings_post_type()
         $cerber_main_value['dateformat'] = 'd/m/Y g:i:s';
         $cerber_main_value['plain_date'] = 1;
         $cerber_main_value['admin_lang'] = 1;
-        $cerber_main_value['top_admin_menu'] = 0;
-        $cerber_main_value['no_white_my_ip'] = 0;
         update_option('cerber-main', $cerber_main_value);
     }
 
