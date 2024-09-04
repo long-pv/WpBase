0d316386f5f1dc030ca68df32fb8e6106afd7c05
LongPV <longpv@codluck.com>
2024-09-04 15:27:36 +0700

    tối ưu phân trang

--- wp-content/themes/wpbase/inc/pagination.php
+++ wp-content/themes/wpbase/inc/pagination.php
@@ -1,33 +1,18 @@
 <?php
 function pagination($query = null)
 {
-    $max_pages = 0;
-    $prev_text = __('Prev', 'basetheme');
-    $next_text = __('Next', 'basetheme');
-
-    if ($query) {
-        $max_pages = $query->max_num_pages;
-    } else {
-        global $wp_query;
-        $max_pages = $wp_query->max_num_pages;
-    }
+    global $wp_query;
+    $max_pages = $query ? $query->max_num_pages : $wp_query->max_num_pages;
 
     echo '<div class="pagination">';
     echo paginate_links(
         array(
-            'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
             'total' => $max_pages,
             'current' => max(1, get_query_var('paged')),
-            'format' => '?paged=%#%',
-            'show_all' => false,
-            'type' => 'plain',
             'end_size' => 2,
             'mid_size' => 1,
-            'prev_next' => true,
-            'prev_text' => $prev_text,
-            'next_text' => $next_text,
-            'add_args' => false,
-            'add_fragment' => '',
+            'prev_text' => __('Prev', 'basetheme'),
+            'next_text' => __('Next', 'basetheme'),
         )
     );
     echo '</div>';
