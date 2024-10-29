92d0bbe1529f8fa4e381012fefdd68df83fe7383
longpv <longpv@codluck.com>
2024-10-29 22:06:04 +0700

    size ảnh

--- wp-content/themes/wpbase/inc/template-functions.php
+++ wp-content/themes/wpbase/inc/template-functions.php
@@ -13,3 +13,16 @@ if (function_exists('acf_add_options_page')) {
         )
     );
 }
+
+/**
+ * Add Recommended size image to Featured Image Box    
+ */
+add_filter('admin_post_thumbnail_html', 'add_featured_image_instruction');
+function add_featured_image_instruction($html)
+{
+    if (get_post_type() === 'post') {
+        $html .= '<p>Recommended size: 300x300</p>';
+    }
+
+    return $html;
+}
