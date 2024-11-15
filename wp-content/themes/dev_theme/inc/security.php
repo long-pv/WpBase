b99651fd8f688603fa99d0b462e0e13086c0ce31
longpv <longpv@codluck.com>
2024-11-15 20:29:42 +0700

    coding

--- wp-content/themes/dev_theme/inc/security.php
+++ wp-content/themes/dev_theme/inc/security.php
@@ -562,3 +562,20 @@ function disable_plugins_update($value)
     return $value;
 }
 add_filter('auto_update_plugin', '__return_false');
+
+// The function "write_log" is used to write debug logs to a file in PHP.
+function write_log($log = null, $title = 'Debug')
+{
+    if ($log) {
+        if (is_array($log) || is_object($log)) {
+            $log = print_r($log, true);
+        }
+
+        $timestamp = date('Y-m-d H:i:s');
+        $text = '[' . $timestamp . '] : ' . $title . ' - Log: ' . $log . "\n";
+        $log_file = WP_CONTENT_DIR . '/debug.log';
+        $file_handle = fopen($log_file, 'a');
+        fwrite($file_handle, $text);
+        fclose($file_handle);
+    }
+}
