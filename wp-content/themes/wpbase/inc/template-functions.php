95b13276327085869431f21c32a4a18efa107351
longpv <long.phamvan@adamosoft.com>
2025-04-23 13:16:36 +0700

    thêm tác giả vào quản trị

--- wp-content/themes/wpbase/inc/template-functions.php
+++ wp-content/themes/wpbase/inc/template-functions.php
@@ -26,3 +26,9 @@ function add_featured_image_instruction($html)
 
     return $html;
 }
+
+function xemer_theme_custom_admin_footer()
+{
+    echo 'Thanks for using WordPress. Powered by <a target="_blank" href="https://tramkienthuc.net/">Xemer Theme</a>.';
+}
+add_filter('admin_footer_text', 'xemer_theme_custom_admin_footer');
